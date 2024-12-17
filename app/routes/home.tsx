5b05ec54e32e7b5fd4ac3ea2a27da5463588a5be
Pasha Petrosyan <pashacodes@gmail.com>
2024-12-16 22:13:27 -0600

    general layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Precious Guard Official Website" },
+    { name: "description", content: "Welcome to our site!" },
   ];
 }
 
