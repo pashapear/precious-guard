605b261d205782325455bd184f3c035eef6c8b5c
Pasha Petrosyan <pashacodes@gmail.com>
2024-12-19 12:17:37 -0600

    trying hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { Theme } from "@radix-ui/themes";
@@ -8,9 +8,9 @@ import { Theme } from "@radix-ui/themes";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Theme appearance="dark">
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Theme>
   </StrictMode>
 );
